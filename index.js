05a2e9bf49feff7d0cc72c92a11794cd989c33ec
RobinHossain5261 <robinhossain5261.com>
2023-04-14 03:29:52 +0600

    job catagory dynamic done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,15 +12,34 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nbqc6.mongodb.net/?retryWrites=true&w=majority`;
 
-
-const uri = "mongodb+srv://<username>:<password>@cluster0.nbqc6.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+async function run() {
+    try {
+
+        const allJobsCollection = client.db('Job-portal').collection('allJobs');
+
+        app.get('/allJobs', async (req, res) => {
+            const query = {};
+            const options = await allJobsCollection.find(query).toArray();
+            res.send(options);
+        })
+
+        app.get('/allJobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const jobs = await allJobsCollection.findOne(query);
+            res.send(jobs)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 app.get('/', async (req, res) => {
